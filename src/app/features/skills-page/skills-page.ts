86fd8fd5217ece558321f9f2c4d3e11afb1bf908
HectorDaniel-00 <danielvargas.nike@gmail.com>
2026-05-22 12:50:38 -0500

    refactor(skills-page): integrar componente grid reutilizable

--- src/app/features/skills-page/skills-page.ts
+++ src/app/features/skills-page/skills-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { skillGroups } from '@shared/data/portfolio.data';
+import { GridSkillsComponet } from '@shared/components/grid-skills-componet/grid-skills-componet';
 
 @Component({
   selector: 'skills-page',
-  imports: [],
+  imports: [GridSkillsComponet],
   templateUrl: './skills-page.html',
 })
 export class SkillsPage {
